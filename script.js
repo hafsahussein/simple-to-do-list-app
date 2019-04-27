a55016690d1d1f04ce0105cc573b84d8acd84029
hafsahussein <49996540+hafsahussein@users.noreply.github.com>
2019-04-27 11:41:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
 function addTask() {
-	var table =document.getElementsByTagName("table")[0];
-	var task=document.getElementById("text").value;
-	
-	for(var i=1;i<table.rows.length;i++) {
-		var existence=table.rows[i].cells[0].innerHTML;	
-		console.log(existence);
-	}
-	if(task==existence)
-			alert("task already exists");
-		else if(task.trim()=="") 
-			alert("fill the task");
+    var table =document.getElementsByTagName("table")[0];
+    var task=document.getElementById("text").value;
+    
+    for(var i=1;i<table.rows.length;i++) {
+        var existence=[]; existence[i]=table.rows[i].cells[0].innerHTML;  
+          if(task==existence[i])
+          var exists=true;
+    }
+    if(exists)
+            alert("task already exists!");
+        else if(task.trim()=="") 
+            alert("fill the task please!");
 
-	else {
-	var row=table.insertRow();
-	var cell1=row.insertCell();
-	var cell2=row.insertCell();
-	cell1.innerHTML=task;
-	cell2.innerHTML="X";
-    document.getElementById("text").value="";	
-		}
-	for(var i=1;i<table.rows.length;i++) {
-		table.rows[i].cells[1].onclick=function() {
-			var index=this.parentElement.rowIndex;
-			table.deleteRow(index);
-		}
-	}
-	}
-		
-	
+    else {
+    var row=table.insertRow();
+    var cell1=row.insertCell();
+    var cell2=row.insertCell();
+    cell1.innerHTML=task;
+    cell2.innerHTML="X";
+    document.getElementById("text").value="";    
+        }
+    for(var i=1;i<table.rows.length;i++) {
+        table.rows[i].cells[1].onclick=function() {
+            var index=this.parentElement.rowIndex;
+            table.deleteRow(index);
+        }
+    }
+    }
+        
+    
